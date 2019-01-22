acc73e5fa4cc7b98bb7fac655b06de25f1c84765
wmctavish <31712694+wmctavish@users.noreply.github.com>
2019-01-22 15:08:07 -0500

    Add files via upload

---
+++ public/library.js
@@ -0,0 +1,154 @@
+// Passes section
+
+$('#passing').click(()=>{
+   $('#passing-options').toggle();
+});
+
+$('#x-pass').click(()=>{
+   $('#x-passes').toggle();
+});
+
+$('#standxpass').click(()=>{
+   $('#standxpassvid').toggle();
+});
+
+$('#kneecut').click(()=>{
+   $('#kneecuts').toggle();
+});
+
+$('#lepriknee').click(()=>{
+   $('#leprikneevid').toggle();
+});
+
+$('#domknee').click(()=>{
+   $('#domkneevid').toggle();
+});
+
+$('#toreando').click(()=>{
+   $('#toreandos').toggle();
+});
+
+$('#jonttoreando').click(()=>{
+   $('#jonttoreandovid').toggle();
+});
+
+$('#lassopass').click(()=>{
+   $('#lassopasses').toggle();
+});
+
+$('#leprilassopass').click(()=>{
+   $('#leprilassopassvid').toggle();
+});
+
+
+// Submissions section
+
+$('#submissions').click(()=>{
+   $('#submission-options').toggle();
+});
+
+$('#chokes').click(()=>{
+   $('#choke-options').toggle();
+});
+
+$('#triangles').click(()=>{
+   $('#triangle-options').toggle();
+});
+
+$('#3setups').click(()=>{
+   $('#3setupsvid').toggle();
+});
+
+$('#darces').click(()=>{
+   $('#darce-options').toggle();
+});
+
+$('#3darcesetups').click(()=>{
+   $('#3darcesetupsvid').toggle();
+});
+
+$('#escapedarce').click(()=>{
+   $('#escapedarcevid').toggle();
+});
+
+$('#nsdarce').click(()=>{
+   $('#nsdarcevid').toggle();
+});
+
+$('#locks').click(()=>{
+   $('#lock-options').toggle();
+});
+
+$('#halfguardkimura').click(()=>{
+   $('#halfguardkimuravid').toggle();
+});
+
+$('#abkimcombo').click(()=>{
+   $('#abkimcombovid').toggle();
+});
+
+$('#legs').click(()=>{
+   $('#leg-options').toggle();
+});
+
+$('#straightankle').click(()=>{
+   $('#straightanklevid').toggle();
+});
+
+
+
+
+// Guard section
+$('#guard').click(()=>{
+   $('#guard-options').toggle();
+});
+
+$('#guard-concepts').click(()=>{
+   $('#concept-options').toggle();
+});
+
+$('#keenan-retention').click(()=>{
+   $('#keenan-retentionvid').toggle();
+});
+
+$('#jon-retention').click(()=>{
+   $('#jon-retentionvid').toggle();
+});
+
+$('#sweeps').click(()=>{
+   $('#sweep-options').toggle();
+});
+
+$('#tomoenage').click(()=>{
+   $('#tomoenagevid').toggle();
+});
+
+$('#4sweeps').click(()=>{
+   $('#4sweepsvid').toggle();
+});
+
+
+// Escapes section
+$('#escapes').click(()=>{
+   $('#escape-options').toggle();
+});
+
+$('#mount-escapes').click(()=>{
+   $('#mount-escape-options').toggle();
+});
+
+$('#elbowescape').click(()=>{
+   $('#elbowescapevid').toggle();
+});
+
+$('#mounttoslx').click(()=>{
+   $('#mounttoslxvid').toggle();
+});
+
+$('#sidecontrol-escapes').click(()=>{
+   $('#sidecontrol-escape-options').toggle();
+});
+
+$('#jonscescape').click(()=>{
+   $('#jonscescapevid').toggle();
+});
